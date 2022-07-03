1fb958a79721dca1ce5604341fadb62a66c2d13b
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-07-03 21:21:26 +0600

    add simple function

--- hello.py
+++ hello.py
@@ -1,3 +1,23 @@
-username = input("your name: ")
+# username = input("your name: ")
+#
+# print(f"Hello {username}!")
 
-print(f"Hello {username}!")
+# def add_two_value(first,second):
+#     return first + second
+#
+# number_1 =10
+# number_2 =5
+# sum = add_two_value(number_1,number_2)
+#
+# print(f"{number_1}+{number_2}={sum}")
+
+def add_three_value(first, second, third):
+    return first * second * third
+
+num_1 =10
+num_2 =20
+num_3 =30
+multiple = add_three_value(num_1, num_2, num_3)
+
+print(f"{num_1} * {num_2} * {num_3} = {multiple}")
+
